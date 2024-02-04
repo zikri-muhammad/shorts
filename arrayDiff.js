b023dffa0e2e7c6c05589381b3345d5ab2b14ba5
zikri-muhammad <muhammadzikri598@gmail.com>
2024-02-04 17:14:14 +0700

    feat(array diff)

---
+++ arrayDiff.js
@@ -0,0 +1,23 @@
+function arrayDiff(a, b) {
+    // let arry = []
+    // let diff = 0
+    // for (let i = 0; i < a.length; i++) {
+    //     const element = a.includes(b[i]);
+    //     console.log(element)
+    //     if (element == true) {
+    //         arry.push(a[i])
+            
+    //     } 
+        
+    // }
+    // return arry
+    return a.filter(element => !b.includes(element));
+}
+
+
+// console.log(arrayDiff([1,2], [2]))
+console.log(arrayDiff([1,2,2,2,3],[2]))
+console.log(arrayDiff([1,2,3],[1,2]))
+// console.log(arrayDiff([1,2,2],[2]))
+// console.log(arrayDiff([1,2,2],[]))
+// console.log(arrayDiff([1,2,2,2,3],[2]))
