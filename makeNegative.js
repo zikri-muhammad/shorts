92cc497a93c6df62cf8fae490e8e705b49f95aba
zikri-muhammad <muhammadzikri598@gmail.com>
2024-02-06 15:23:22 +0700

    feat(make negative) solve problem make negative

---
+++ makeNegative.js
@@ -0,0 +1,9 @@
+function makeNegative(num) {
+    // Code?
+    if(num <= 0) return num
+    return -num
+}
+  
+console.log(makeNegative(-9))
+console.log(makeNegative(42))
+console.log(makeNegative(50))
