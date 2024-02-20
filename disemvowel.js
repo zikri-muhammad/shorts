563fcbc0f3356ab614eb7c68b532080305df4502
zikri-muhammad <muhammadzikri598@gmail.com>
2024-02-20 17:56:24 +0700

    feat(disemvowel) disemvowel

---
+++ disemvowel.js
@@ -0,0 +1,15 @@
+function disemvowel(str) {
+    let vowel = ['a', 'i', 'u', 'e', 'o', 'A', 'I', 'U', 'E', 'O'];
+    let str1 = ''
+    for (let i = 0; i < str.length; i++) {
+        const element = str[i];     
+        console.log(element);
+        if (!vowel.includes(element)) {
+            str1 += element
+        }
+    }
+    return str1
+}
+console.log(disemvowel('Ths wbst s fr lsrs LOL!'))
+console.log(disemvowel('ohOH uevycdijAHBwjeQpa WREJSeAAmuru iqaflGllKTaOxOLvYZ buQAeeiDykmYHIiMOV pCNhCGMzZFhAVse cKsaro nmaAJvJ hPHUxUNEasoAt ONvqcFoxf'))
+// Ths wbst s fr lsrs LL!
